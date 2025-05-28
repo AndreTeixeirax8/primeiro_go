e9f93b0e29b16f087c2579531ecbc9ca969704b7
André Teixeira <andre-x8@hotmail.com>
2025-05-28 08:55:00 -0300

    aula 15 repository

---
+++ internal/infra/database/repository/cadastro/unidade.go
@@ -0,0 +1,48 @@
+package repository
+
+import (
+	"github.com/primeiro/internal/modules/cadastro/domain/entity"
+	"gorm.io/gorm"
+)
+
+type UnidadeRepository struct {
+	Db *gorm.DB
+}
+
+func NewUnidadeRepository(db *gorm.DB) *UnidadeRepository {
+	return &UnidadeRepository{
+		Db: db,
+	}
+}
+
+func (r *UnidadeRepository) Create(unidade *entity.Unidade) error {
+
+	return r.Db.Create(unidade).Error
+}
+
+func (r *UnidadeRepository) GetByID(id string) (*entity.Unidade, error) {
+	var unidade entity.Unidade
+	err := r.Db.First(&unidade, "id = ?", id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &unidade, nil
+}
+
+func (r *UnidadeRepository) GetAll() ([]entity.Unidade, error) {
+	var unidades []entity.Unidade
+	err := r.Db.Find(&unidades).Error
+	if err != nil {
+		return nil, err
+	}
+	return unidades, nil
+}
+
+func (r *UnidadeRepository) Update(unidade *entity.Unidade) error {
+	return r.Db.Updates(unidade).Error
+}
+
+func (r *UnidadeRepository) Delete(id string) error {
+
+	return r.Db.Delete(&entity.Unidade{}, "id = ?", id).Error
+}
---
+++ internal/modules/cadastro/domain/repository/unidade.go
@@ -0,0 +1,12 @@
+package domain
+
+import (
+	"github.com/primeiro/internal/modules/cadastro/domain/entity"
+)
+
+type UnidadeRepository interface {
+	Create(unidade *entity.Unidade) (*entity.Unidade, error)
+	GetByID(id string) (*entity.Unidade, error)
+	Update(unidade *entity.Unidade) (*entity.Unidade, error)
+	Delete(id string) error
+}
